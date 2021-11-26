3d926a7763066377b29758e031d6ee897aa50b64
sxrgiocs <scarrerasal@gmail.com>
2021-11-27 00:19:36 +0100

    Fixed problem with handlers

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -54,7 +54,6 @@ end
 local servers = {
     "python",
     "latex",
-    "lua",
 }
 
 for _, lsp in ipairs(servers) do
@@ -77,10 +76,7 @@ lspSymbol("Information", "")
 lspSymbol("Hint", "")
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-   virtual_text = {
-      prefix = "",
-      spacing = 0,
-   },
+   virtual_text = false;
    signs = true,
    underline = true,
    -- set this to true if you want diagnostics to show in insert mode
@@ -102,34 +98,3 @@ end
 -- show diagnostics on hover
 vim.o.updatetime = 250
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false})]]
-
--- show lsp source
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  function(_, _, params, client_id, _)
-    local config = { -- your config
-      underline = true, --hide underline as
-      virtual_text = false, -- hide virtual text which is annoying af
-      signs = true,
-      update_in_insert = false,
-    }
-    local uri = params.uri
-    local bufnr = vim.uri_to_bufnr(uri)
-
-    if not bufnr then
-      return
-    end
-
-    local diagnostics = params.diagnostics
-
-    for i, v in ipairs(diagnostics) do
-      diagnostics[i].message = string.format("%s: %s", v.source, v.message)
-    end
-
-    vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
-
-    if not vim.api.nvim_buf_is_loaded(bufnr) then
-      return
-    end
-
-    vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
-  end
